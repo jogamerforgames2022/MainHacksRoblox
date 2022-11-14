a10c1cc1a1183ed88d65002e44d8830494fc1874
ShadowSlime <108364528+jogamerforgames2022@users.noreply.github.com>
2022-11-14 22:22:34 +0200

    Update DoorsLatestUpdateLoader.lua

--- DoorsLatestUpdateLoader.lua
+++ DoorsLatestUpdateLoader.lua
@@ -1,2 +1,2 @@
 --3.00
-loadstring(game:HttpGet('https://raw.githubusercontent.com/jogamerforgames2022/MainHacksRoblox/main/Doors3-0.lua'))()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/jogamerforgames2022/MainHacksRoblox/main/Protected.lua'))()
